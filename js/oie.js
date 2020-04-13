b96f9da07bde8858fb61d290534e90ad004cefd1
nathan <cerisara.nathan@gmail.Com>
2020-04-13 10:59:09 +0200

    aa

--- js/oie.js
+++ js/oie.js
@@ -57,7 +57,7 @@ function update_aff_js(){
 function init_joueurs(){
     var parameters = location.search.substring(1).split("&");
     for(p of parameters){
-        var jj={name:null,img:null,case:0,actif:null};
+        var jj={name:null,img:null,case:0,actif:null,nblances:0};
         var pjs=p.split(",");
         for(pp of pjs){
             var ppp=pp.split("=");
@@ -107,7 +107,8 @@ function initialisation() {
     
 }
 
-function suppr_actif(idp){
+function suppr_actif(idp=null){
+    if(idp==null){ idp=jact; }
     /*var caseActiveChaine = (caseActive < 10 ? '0' : '') + caseActive;
     if (caseActive >= 0 && caseActive <= 63) {
         //alert(caseActiveChaine);
@@ -121,7 +122,8 @@ function suppr_actif(idp){
     var elementActif = document.getElementById("item-" + caseActiveChaine);
     elementActif.setAttribute("class", "flexbox-item item-" + caseActiveChaine);
     */
-    var caseActiveChaine = (joueurs[idp].case < 10 ? '0' : '') + caseActive;
+    //alert("suppr actif : case = "+joueurs[jact].case);
+    var caseActiveChaine = (joueurs[idp].case < 10 ? '0' : '') + joueurs[idp].case;
     var elem=document.getElementById("item-"+caseActiveChaine);
     try{
         elem.removeChild(joueurs[idp].actif);
@@ -132,7 +134,8 @@ function suppr_actif(idp){
 
 }
 
-function make_actif(idp){
+function make_actif(idp=null){
+    if(idp==null){ idp=jact; }
     /*
     if (caseActive >= 0) {
         caseActiveChaine = (caseActive < 10 ? '0' : '') + caseActive;
@@ -140,23 +143,56 @@ function make_actif(idp){
         elementActif.setAttribute("class", "flexbox-item item-" + caseActiveChaine + " active");
     };
     */
-   var caseActiveChaine = (joueurs[idp].case < 10 ? '0' : '') + caseActive;
-   var elem=document.getElementById("item-"+caseActiveChaine);
-   elem.appendChild(joueurs[idp].actif);
+    //alert("make actif : case = "+joueurs[jact].case);
+    var caseActiveChaine = (joueurs[idp].case < 10 ? '0' : '') + joueurs[idp].case;
+    var elem=document.getElementById("item-"+caseActiveChaine);
+    if(elem!=null){
+        elem.appendChild(joueurs[idp].actif);
+    }
 }
 
-
-function deplacement_oie(caseActive,nb_tot,pas=1){
-    var cc=caseActive;
-    console.log("deplacement oie( cA : "+cc+" , nb_tot : "+nb_tot+" , pas : "+pas);
+/*
+function deplacement_oie(debut,fin,pas=1){
+    var jact=jact;
+    //alert(jact);
+    var cc=debut;
+    console.log("deplacement oie( debut : "+debut+" , fin : "+fin+" , pas : "+pas);
     
     var nba=0;
     function boucle_dep(){
         nba+=pas;
-        suppr_actif(jact);
+        suppr_actif();
         cc+=1;
-        make_actif(jact);
-        if(nba!=nb_tot){
+        joueurs[jact].case=cc;
+        make_actif();
+        if(cc!=fin){
+            sleep(300).then(() => {
+                window.requestAnimationFrame(boucle_dep);
+            })
+        }
+    }
+    window.requestAnimationFrame(boucle_dep);
+}
+*/
+
+function deplacement_oie(debut,fin,pas=1){
+    //alert("debut : "+debut+" , fin : "+fin);
+    //alert("tout debut deplacement oie : case = "+joueurs[jact].case);
+    suppr_actif();
+    joueurs[jact].case=debut;
+    make_actif();
+    //alert("debut deplacement oie : case = "+joueurs[jact].case);
+    function boucle_dep(){
+        //alert("etape 1 deplacement oie : case = "+joueurs[jact].case);
+        suppr_actif();
+        if(pas==1){ joueurs[jact].case++; }
+        else if(pas==-1){ joueurs[jact].case--; }
+        else{ joueurs[jact].case=joueurs[jact].case+pas; }
+        make_actif();
+        update_aff_js();
+        //alert("etape 2 deplacement oie : case = "+joueurs[jact].case);
+        console.log("case : "+joueurs[jact].case);
+        if(joueurs[jact].case!=fin){
             sleep(300).then(() => {
                 window.requestAnimationFrame(boucle_dep);
             })
@@ -192,7 +228,7 @@ async function lancer_des(){
                 av+=randint(1,5);
             }
             sleep(td).then(() => {
-                window.requestAnimationFrame(boucle);
+                //window.requestAnimationFrame(boucle);
             })
         }
         else{
@@ -209,10 +245,10 @@ async function jeu(){
     await lancer_des();
     var de1=parseInt(document.getElementById("dé1").innerHTML)
     var de2=parseInt(document.getElementById("dé2").innerHTML)
-    alert(de1+" "+de2);
-    nblances++;
+    //alert(de1+" "+de2);
+    joueurs[jact].nblances++;
     document.getElementById("lancer").innerHTML = "(" + de1 + ", " + de2 + "= " + (de1 + de2) + ")";
-    document.getElementById("nblancers").innerHTML = nblances;
+    document.getElementById("nblancers").innerHTML = joueurs[jact].nblances;
     fonction_1(de1,de2);
 }
 
@@ -222,7 +258,7 @@ const jeu = async () => {
     // do something else here after firstFunction completes
     var de1=parseInt(document.getElementById("dé1").innerHTML)
     var de2=parseInt(document.getElementById("dé2").innerHTML)
-    alert(de1+" "+de2);
+    //alert(de1+" "+de2);
     nblances++;
     document.getElementById("lancer").innerHTML = "(" + de1 + ", " + de2 + "= " + (de1 + de2) + ")";
     document.getElementById("nblancers").innerHTML = nblances;
@@ -237,7 +273,7 @@ function jeu()
     promise.then(function(result) { 
         var de1=parseInt(document.getElementById("dé1").innerHTML)
         var de2=parseInt(document.getElementById("dé2").innerHTML)
-        alert(de1+" "+de2);
+        //alert(de1+" "+de2);
         nblances++;
         document.getElementById("lancer").innerHTML = "(" + de1 + ", " + de2 + "= " + (de1 + de2) + ")";
         document.getElementById("nblancers").innerHTML = nblances;
@@ -257,44 +293,44 @@ function fonction_1(de1=null,de2=null){
             de1=0;
             de2=0;
         }
-        var caseActiveChaine = (caseActive < 10 ? '0' : '') + caseActive;
-        if (nblances == 0 && de1 + de2 == 9) {
+        var caseActiveChaine = (joueurs[jact].case < 10 ? '0' : '') + joueurs[jact].case;
+        if (joueurs[jact].nblances == 0 && de1 + de2 == 9) {
             if ((de1 == 6 && de2 == 3) || (de1 == 3 && de2 == 6)) {
-                caseActive = 29;
+                joueurs[jact].case = 29;
             } else if ((de1 == 4 && de2 == 5) || (de1 == 5 && de2 == 4)) {
-                caseActive = 53;
+                joueurs[jact].case = 53;
             }
         } else {
             //suppr_actif(caseActive);
 
             // On calcule la nouvelle position active
-            caseActive += de1 + de2;
-            if(caseActive<=63){
-                deplacement_oie(caseActive-de1-de2,caseActive,1);
+            joueurs[jact].case += de1 + de2;
+            if(joueurs[jact].case<=63){
+                deplacement_oie(joueurs[jact].case-de1-de2,joueurs[jact].case,1);
             }
-            if(caseActive > 63) {
-                deplacement_oie(caseActive-de1-de2,63,1);
-                var ncas=63 - (caseActive - 63);
+            if(joueurs[jact].case > 63) {
+                deplacement_oie(joueurs[jact].case-de1-de2,63,1);
+                var ncas=63 - (joueurs[jact].case - 63);
                 deplacement_oie(63,ncas,-1);
-                caseActive = ncas;
+                joueurs[jact].case = ncas;
                 //alert("superior " + caseActive)
             }
-            if (caseActive == 63) {
-                alert("Vous avez gagné !")
-                gagne = true;
+            if (joueurs[jact].case == 63) {
+                //alert("Vous avez gagné !")
+                gagne = jact;
             }
         }
         //console.log("caseActive : " + caseActive)
-        if (caseActive <= 63) {
+        if (joueurs[jact].case <= 63) {
             // On recherche le nouvel élément actif.
-            //make_actif(jact);
+            //make_actif();
             //console.log(" caseActiveChaine : "+caseActiveChaine);
         } else {
             elementActif = document.getElementById("item-63");
         }
-        document.getElementById("caseActive").innerHTML = caseActive;
+        document.getElementById("caseActive").innerHTML = joueurs[jact].case;
     } else {
-        alert("Vous avez gagné la partie, pour en recommencer une nouvelle, veuillez recharger la page");
+        //alert("Vous avez gagné la partie, pour en recommencer une nouvelle, veuillez recharger la page");
     }
     jact++;
     if(jact>=joueurs.length){ jact=0; }
